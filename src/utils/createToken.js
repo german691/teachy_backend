d4308580202ea202ae618dbb3cb25f83ff8f026f
Pablo Delgado <slokingdg@gmail.com>
2024-11-20 11:51:15 -0300

    create token utility
    Added utility for generating JWT tokens with configurable key and expiry

---
+++ src/utils/createToken.js
@@ -0,0 +1,20 @@
+import jsonwebtoken from "jsonwebtoken";
+
+const { TOKEN_KEY, TOKEN_EXPIRY } = process.env;
+
+const createToken = async (
+  tokenData,
+  tokenKey = TOKEN_KEY,
+  expiresIn = TOKEN_EXPIRY
+) => {
+  try {
+    const token = jsonwebtoken.sign(tokenData, tokenKey, {
+      expiresIn,
+    });
+    return token;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export default createToken;
